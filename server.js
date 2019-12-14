6851554086dad6aab10a142aabe991351c527363
Guilherme <gui.mochila@gmail.com>
2019-12-14 14:51:37 -0300

    Fix SW handler

--- server.js
+++ server.js
@@ -8,17 +8,15 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 
-const ServiceWorker = app => (req, res) => {
-  const filePath = join(__dirname, '.next', pathname);
-  app.serveStatic(req, res, filePath);
-};
-
 app.prepare().then(() => {
   const server = express();
   server.use(helmet());
-  server.use(handle);
 
-  server.get('/service-worker.js', ServiceWorker(app));
+  // Handling requests to SW
+  server.get('/service-worker.js', (req, res) => {
+    const filePath = join(__dirname, '.next', 'service-worker.js');
+    res.sendFile(filePath);
+  });
 
   server.get('*', (req, res) => {
     return handle(req, res);
